b5d7da4b54bee48072915f7a5aab7cdeeb89f345
akash kumar <111784316+akashkumar8115@users.noreply.github.com>
2024-05-23 22:46:53 +0530

    ok

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function toggle() {
+    document.getElementById("uls").style.display = "block";
+    document.getElementById("uls").style.marginLeft = "-52vw !important";
+    document.getElementById("toggle").style.display = "none";
+    document.getElementById("cross").style.display = "block";
+}
+function cross() {
+    document.getElementById("uls").style.display = "none";
+    // document.getElementById("uls").style.marginLeft = "-52vw !important";
+    document.getElementById("toggle").style.display = "block";
+    document.getElementById("cross").style.display = "none";
+}
+
+
